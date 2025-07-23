845b88f7ea89af2efb2a4b0ec8375aa2aaad319e
ShawnYen <yen951013@gmail.com>
2025-07-23 13:34:15 +0800

    chore(config): use dotenv to manage

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
 import express from 'express';
 import session from 'express-session';
+import 'dotenv/config';
 import { connectDB } from './database/connect.js';
 import registerRoutes from './routes/index.js'
 const app = express();
 
 app.use(express.json());
 app.use(session({
-	secret: 'your-secret-key',
-	resave: false,
-	saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
 }));
 
 await connectDB();
 registerRoutes(app);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-  console.log('=> http://localhost:3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`=> http://localhost:${process.env.PORT}`);
 });
