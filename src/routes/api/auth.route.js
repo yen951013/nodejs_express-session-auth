a64be374b4c7dcec283ca3cbda5e50ab53c482de
ShawnYen <yen951013@gmail.com>
2025-08-04 15:10:06 +0800

    feat(validation): add schema for login request

--- src/routes/api/auth.route.js
+++ src/routes/api/auth.route.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { validate } from "../../middlewares/validate.middleware.js";
-import { registerSchema } from "../../validations/user.validation.js";
+import { registerSchema, loginSchema } from "../../validations/user.validation.js";
 import register from "../../controllers/auth/register.controller.js";
 import login from "../../controllers/auth/login.controller.js";
 import logout from "../../controllers/auth/logout.controller.js";
 
 const router = Router();
 router.post('/api/auth/register', validate(registerSchema), register);
-router.post('/api/auth/login', login);
+router.post('/api/auth/login', validate(loginSchema), login);
 router.post('/api/auth/logout', logout);
 
 export default router;
--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -43,3 +43,35 @@ export const registerSchema = Joi.object({
       'any.required': '請輸入確認密碼',
     }),
 });
+
+export const loginSchema = Joi.object({
+  userName: Joi.string().trim()
+    .min(5).max(15)
+    .pattern(/^[a-zA-Z0-9_.]+$/)
+    .messages({
+      'string.base': 'userName 必須是文字',
+      'string.empty': 'userName 不可為空',
+      'string.min': 'userName 最少 {#limit} 個字',
+      'string.max': 'userName 最多 {#limit} 個字',
+      'string.pattern.base': 'userName 格式錯誤',
+    }),
+  
+  email: Joi.string().trim()
+    .email()
+    .messages({
+      'string.base': 'email 必須是文字',
+      'string.empty': 'email 不可為空',
+      'string.email': 'email 格式錯誤',
+    }),
+  
+  password: Joi.string().trim()
+    .min(8).max(32)
+    .required()
+    .messages({
+      'string.base': 'password 必須是文字',
+      'string.empty': 'password 不可為空',
+      'string.min': 'password 最少 {#limit} 個字',
+      'string.max': 'password 最多 {#limit} 個字',
+      'any.required': '請輸入 password',
+    }),
+}).or('userName', 'email');
